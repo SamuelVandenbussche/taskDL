9a86dfb2184f0b0e6214084a1664a392cd5ab996
Samuel Vandenbussche <91118298+SamuelVandenbussche@users.noreply.github.com>
2023-12-14 12:42:08 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,24 @@
-import streamlit as st
-from PIL import Image
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
-
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.preprocessing import image
+from keras.utils import image_dataset_from_directory
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import optimizers
 from tensorflow.keras import layers
-from tensorflow.keras.utils import image_dataset_from_directory
-from tensorflow.keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import os
+import matplotlib.pyplot as plt
 
 
 # Define the categories
