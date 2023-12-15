fe704b29a9a5d0d98cdf5c45ee88cb71d70244d2
Samuel Vandenbussche <91118298+SamuelVandenbussche@users.noreply.github.com>
2023-12-15 16:29:36 +0100

    Update codeDLTask.py

--- codeDLTask.py
+++ codeDLTask.py
@@ -109,25 +109,3 @@ if st.button('Train the model'):
     fig.tight_layout()
     st.pyplot(fig)
 
-st.header("only upload image when model is trained")
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-# Check if an image has been uploaded
-if uploaded_file is not None:
-    # Display the uploaded image
-    st.image(uploaded_file, caption="Uploaded Image.", width=300)
-
-    # Convert the uploaded image to a format compatible with the model
-    img = Image.open(uploaded_file).resize((128, 128))
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-
-    # Make predictions
-    predictions = st.session_state.model.predict(img_array)
-    class_labels = st.session_state.train_set.class_names
-    predicted_category = class_labels[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0]) * 100
-
-    # Display predictions
-    st.subheader("Prediction:")
-    st.write(f"The uploaded image is most likely a '{predicted_category}' with {confidence:.2f}% confidence.")
