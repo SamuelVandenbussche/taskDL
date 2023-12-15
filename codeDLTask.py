25820d5901272bbd4177b134596d9d37d6c39e64
Samuel Vandenbussche <91118298+SamuelVandenbussche@users.noreply.github.com>
2023-12-15 13:16:13 +0100

    Update codeDLTask.py

--- codeDLTask.py
+++ codeDLTask.py
@@ -17,9 +17,9 @@ st.write("This application is a deep learnign model for classifying a image as a
 
 st.header("EDA")
 # Display images
-st.image("EDA.JPG", width=600)
+st.image("EDA.JPG", width=700)
 # Display images
-st.image(["1d50dc8500.jpg", "3a3a3b38f4.jpg","1b71d0b173.jpg", "3cda94891a.jpg","1a5c3f9b51.jpg"], width=120)
+st.image(["1d50dc8500.jpg", "3a3a3b38f4.jpg","1b71d0b173.jpg", "3cda94891a.jpg","1a5c3f9b51.jpg"], width=140)
 
 # User input for epochs
 epochs = st.number_input("Enter the number of epochs", min_value=1, value=10, step=1)
